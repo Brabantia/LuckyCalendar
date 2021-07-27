c9973024f470755bb48c19219689248e56096037
Brabantia <45050586+Brabantia@users.noreply.github.com>
2021-07-27 04:31:01 -0700

    Implemented model, and file loading and saving functionality (#3)

--- Source/LuckyCalendar.java
+++ Source/LuckyCalendar.java
@@ -5,12 +5,16 @@
  *	@version 1.00 2021/7/17
 **/
 
-public class LuckyCalendar {
-	public static final String DATA_PATH = "";
+import java.io.IOException;
 
-	public static void main(String[] args) {
+public class LuckyCalendar {
+	public static final String DATA_PATH = "input.txt";
+
+	public static void main(String[] args) throws IOException {
 		CalendarModel model = new CalendarModel();
-		model.addFromFile(DATA_PATH);
+		if (!model.addFromFile(DATA_PATH)) {
+			System.out.println("Failed to load from file: " + DATA_PATH);
+		}
 
 		FrameView frame = new FrameView();
 
