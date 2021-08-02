4fcc9857735b7bf539ac53f65563851bc944cfdc
Brabantia <yvandewater@gmail.com>
2021-08-02 04:50:25 -0700

    Move month navigation buttons back into minicalendar (#11)
    Original, the month navigation buttons were part of the minicalendar. I removed this because I felt all buttons should be at the top frame level. But wanting to add the minicalendar to the create events dialog, I realized this was a mistake. 
    
    - Restore month navigation buttons
    - add today button to minicalendar
    - remove these buttons from the top level frame
    - Converted the month to a combo box and the year to a slider to ease navigation

--- Source/MiniCalendarView.java
+++ Source/MiniCalendarView.java
@@ -2,23 +2,31 @@
  *	@(#)MiniCalendarView.java
  *
  *	@author Shyam Vyas
- *	@version 1.00 2021/7/27
+ *	@author Yorick van de Water
+ *	@version 1.00 2021/8/2
 **/
 
-import java.awt.Color;
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.time.LocalDate;
+import java.time.Month;
 
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.SwingConstants;
 
 public class MiniCalendarView extends JPanel {
 	private Controller controller;
 	private LocalDate date;
 
-	public MiniCalendarView() {
+	public MiniCalendarView(LocalDate date) {
 		super(new BorderLayout());
-		this.date = LocalDate.now();
+		this.date = date;
 		updateCalendar();
 	}
 
@@ -27,8 +35,46 @@ public class MiniCalendarView extends JPanel {
 	}
 
 	private void updateCalendar() {
+		SpinnerNumberModel yearModel = new SpinnerNumberModel(this.date.getYear(), 0, 3000, 1);
+		JComboBox<Month> months = new JComboBox<Month>(Month.values());
+		JSpinner years = new JSpinner(yearModel);
+		JButton today = new JButton("today");
+		JButton left = new JButton("<");
+		JButton right = new JButton(">");
+
+		today.setBackground(Color.WHITE);
+		left.setBackground(Color.WHITE);
+		right.setBackground(Color.WHITE);
+		months.setBackground(Color.WHITE);
+		months.setSelectedItem(this.date.getMonth());
+		years.setBackground(Color.WHITE);
+		years.setEditor(new JSpinner.NumberEditor(years, "#"));
+
+		today.addActionListener(event -> {
+			this.controller.setDate(LocalDate.now());
+		});
+		left.addActionListener(event -> {
+			this.controller.setDate(this.date.minusMonths(1));
+		});
+		months.addActionListener(event -> {
+			this.controller.setDate(this.date.withMonth(months.getItemAt(months.getSelectedIndex()).getValue()));
+		});
+		right.addActionListener(event -> {
+			this.controller.setDate(this.date.plusMonths(1));
+		});
+		years.addChangeListener(event -> {
+			this.controller.setDate(this.date.withYear(yearModel.getNumber().intValue()));
+		});
+
 		super.removeAll();
-		super.add(new JLabel(this.date.getMonth() + " " + this.date.getYear(), SwingConstants.CENTER), BorderLayout.CENTER);
+		JPanel panel = new JPanel();
+		panel.add(today);
+		panel.add(left);
+		panel.add(months);
+		panel.add(right);
+		panel.add(years);
+
+		super.add(panel, BorderLayout.PAGE_START);
 		super.add(getChildPanel(), BorderLayout.PAGE_END);
 		super.revalidate();
 	}
@@ -40,13 +86,13 @@ public class MiniCalendarView extends JPanel {
 		int maxDay = this.date.lengthOfMonth();
 
 		panel.setLayout(new GridLayout(7,7));
-		panel.add(new JLabel("Su"));
-		panel.add(new JLabel("Mo"));
-		panel.add(new JLabel("Tu"));
-		panel.add(new JLabel("We"));
-		panel.add(new JLabel("Th"));
-		panel.add(new JLabel("Fr"));
-		panel.add(new JLabel("Sa"));
+		panel.add(new JLabel("Su", SwingConstants.CENTER));
+		panel.add(new JLabel("Mo", SwingConstants.CENTER));
+		panel.add(new JLabel("Tu", SwingConstants.CENTER));
+		panel.add(new JLabel("We", SwingConstants.CENTER));
+		panel.add(new JLabel("Th", SwingConstants.CENTER));
+		panel.add(new JLabel("Fr", SwingConstants.CENTER));
+		panel.add(new JLabel("Sa", SwingConstants.CENTER));
 
 		int counter = 0;
 		while (counter++ < day.getDayOfWeek().getValue() % 7) {
