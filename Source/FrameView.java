cb2a5f4fa61c59067770102c44d3b3ed83477389
Yorick van de Water <yvandewater@gmail.com>
2021-08-03 06:04:19 -0700

    Create Event dialog

--- Source/FrameView.java
+++ Source/FrameView.java
@@ -13,15 +13,26 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
 import java.time.LocalDate;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SpinnerModel;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.SwingConstants;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class FrameView extends JFrame {
@@ -35,6 +46,8 @@ public class FrameView extends JFrame {
 	private CalendarView currentView;
 	private Controller controller;
 	private LocalDate date;
+	private LocalDate eventStartDate;
+	private LocalDate eventEndDate;
 
 	public FrameView() {
 		this.date = LocalDate.now();
@@ -106,22 +119,202 @@ public class FrameView extends JFrame {
 	}
 
 	private void createEvent() {
-		String name = JOptionPane.showInputDialog("Enter the date name");
-		if (name == null) {
-			return;
-		}
-		String date = JOptionPane.showInputDialog("Enter the date [mm/dd/yyyy]");
-		String startTime = JOptionPane.showInputDialog("Enter the start time [HH:mm]");
-		String endTime = JOptionPane.showInputDialog("Enter the end time [HH:mm]");
-		TimeInterval timeInterval = new TimeInterval(startTime, endTime);
-		Event event = new Event(name, date, timeInterval);
+		SpinnerModel startHourModel = new SpinnerNumberModel(0, 0, 23, 1);
+		SpinnerModel startMinuteModel = new SpinnerNumberModel(0, 0, 45, 15);
+		SpinnerModel endHourModel = new SpinnerNumberModel(0, 0, 23, 1);
+		SpinnerModel endMinuteModel = new SpinnerNumberModel(0, 0, 45, 15);
+		JDialog dialog = new JDialog(this, "Create Event", true);
+				
+		JButton addAttendee = new JButton("Add");
+		JButton cancel = new JButton("Cancel");
+		JButton create = new JButton("Create");
+		JCheckBox friday = new JCheckBox();
+		JCheckBox isAvailable = new JCheckBox();
+		JCheckBox isRecurring = new JCheckBox();
+		JCheckBox monday = new JCheckBox();
+		JCheckBox saturday = new JCheckBox();
+		JCheckBox sunday = new JCheckBox();
+		JCheckBox thursday = new JCheckBox();
+		JCheckBox tuesday = new JCheckBox();
+		JCheckBox wednesday = new JCheckBox();
+		JLabel startDateLabel = new JLabel("Start Date");
+		JSpinner endHourSpinner = new JSpinner(endHourModel);
+		JSpinner endMinuteSpinner = new JSpinner(endMinuteModel);
+		JSpinner startMinuteSpinner = new JSpinner(startMinuteModel);
+		JSpinner startHourSpinner = new JSpinner(startHourModel);
+		JTextArea description = new JTextArea(4, 60);
+		JTextField attendee = new JTextField(35);
+		JTextField eventName = new JTextField(20);
+		JTextField location = new JTextField(50);
+		MiniCalendarView endDateCal = new MiniCalendarView(this.date);
+		MiniCalendarView startDateCal = new MiniCalendarView(this.date);
+		
+		startDateCal.attach(new Controller(null, null) {
+			public void setDate(LocalDate date) {
+				eventStartDate = date;
+				startDateCal.setDate(date);
+			}
+		});
+		endDateCal.attach(new Controller(null, null) {
+			public void setDate(LocalDate date) {
+				eventEndDate = date;
+				endDateCal.setDate(date);
+			}
+		});
 
-		Event conflict = this.controller.createEvent(event);
-		if (conflict == null) {
-			JOptionPane.showMessageDialog(null, "Add Success!");
-		} else {
-			JOptionPane.showMessageDialog(null, "There's a conflicting event: " + conflict);
-		}
+		dialog.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.insets = new Insets(20,10,1,10);
+		c.gridx = 0;
+		c.gridy = 0;
+		c.anchor = GridBagConstraints.LINE_END;
+		dialog.add(new JLabel("Event Name", SwingConstants.RIGHT), c);
+		c.gridx = 1;
+		c.anchor = GridBagConstraints.LINE_START;
+		dialog.add(eventName, c);
+		c.gridx = 2;
+		c.anchor = GridBagConstraints.LINE_END;
+		dialog.add(new JLabel("Recurring", SwingConstants.RIGHT), c);
+		c.gridx = 3;
+		c.anchor = GridBagConstraints.LINE_START;
+		dialog.add(isRecurring, c);
+		c.gridx = 0;
+		c.gridy = 1;
+		c.anchor = GridBagConstraints.LINE_END;
+		c.insets = new Insets(1,10,1,10);
+		dialog.add(new JLabel("Start Hour", SwingConstants.RIGHT), c);
+		c.gridx = 1;
+		c.anchor = GridBagConstraints.LINE_START;
+		dialog.add(startHourSpinner, c);
+		c.gridx = 2;
+		c.anchor = GridBagConstraints.LINE_END;
+		dialog.add(new JLabel("End Hour", SwingConstants.RIGHT), c);
+		c.gridx = 3;
+		c.anchor = GridBagConstraints.LINE_START;
+		dialog.add(endHourSpinner, c);
+		c.gridx = 0;
+		c.gridy = 2;
+		c.anchor = GridBagConstraints.LINE_END;
+		dialog.add(new JLabel("Start Minutes", SwingConstants.RIGHT), c);
+		c.gridx = 1;
+		c.anchor = GridBagConstraints.LINE_START;
+		dialog.add(startMinuteSpinner, c);
+		c.gridx = 2;
+		c.anchor = GridBagConstraints.LINE_END;
+		dialog.add(new JLabel("End Minutes", SwingConstants.RIGHT), c);
+		c.gridx = 3;
+		c.anchor = GridBagConstraints.LINE_START;
+		dialog.add(endMinuteSpinner, c);
+		c.gridx = 0;
+		c.gridy = 3;
+		c.gridwidth = 2;
+		c.anchor = GridBagConstraints.CENTER;
+		dialog.add(startDateLabel, c);
+		c.gridx = 2;
+		dialog.add(new JLabel("End Date", SwingConstants.CENTER), c);
+		c.gridx = 0;
+		c.gridy = 4;
+		dialog.add(startDateCal, c);
+		c.gridx = 2;
+		dialog.add(endDateCal, c);
+		c.gridx = 0;
+		c.gridy = 5;
+		c.gridwidth = 4;
+		dialog.add(new JLabel("Weekly Recurring Event", SwingConstants.CENTER), c);
+		c.gridwidth = 1;
+		c.gridx = 0;
+		c.gridy = 6;
+		c.anchor = GridBagConstraints.LINE_END;
+		dialog.add(new JLabel("Monday", SwingConstants.RIGHT), c);
+		c.gridx = 1;
+		c.anchor = GridBagConstraints.LINE_START;
+		dialog.add(monday, c);
+		c.gridx = 2;
+		c.anchor = GridBagConstraints.LINE_END;
+		dialog.add(new JLabel("Tuesday", SwingConstants.RIGHT), c);
+		c.gridx = 3;
+		c.anchor = GridBagConstraints.LINE_START;
+		dialog.add(tuesday, c);
+		c.gridx = 0;
+		c.gridy = 7;
+		c.anchor = GridBagConstraints.LINE_END;
+		dialog.add(new JLabel("Wednesday", SwingConstants.RIGHT), c);
+		c.gridx = 1;
+		c.anchor = GridBagConstraints.LINE_START;
+		dialog.add(wednesday, c);
+		c.gridx = 2;
+		c.anchor = GridBagConstraints.LINE_END;
+		dialog.add(new JLabel("Thursday", SwingConstants.RIGHT), c);
+		c.gridx = 3;
+		c.anchor = GridBagConstraints.LINE_START;
+		dialog.add(thursday, c);
+		c.gridx = 0;
+		c.gridy = 8;
+		c.anchor = GridBagConstraints.LINE_END;
+		dialog.add(new JLabel("Friday", SwingConstants.RIGHT), c);
+		c.gridx = 1;
+		c.anchor = GridBagConstraints.LINE_START;
+		dialog.add(friday, c);
+		c.gridx = 2;
+		c.anchor = GridBagConstraints.LINE_END;
+		dialog.add(new JLabel("Saturday", SwingConstants.RIGHT), c);
+		c.gridx = 3;
+		c.anchor = GridBagConstraints.LINE_START;
+		dialog.add(saturday, c);
+		c.gridx = 0;
+		c.gridy = 9;
+		c.anchor = GridBagConstraints.LINE_END;
+		dialog.add(new JLabel("Sunday", SwingConstants.RIGHT), c);
+		c.gridx = 1;
+		c.anchor = GridBagConstraints.LINE_START;
+		dialog.add(sunday, c);
+		c.gridx = 0;
+		c.gridy = 10;
+		c.anchor = GridBagConstraints.LINE_END;
+		dialog.add(new JLabel("Location", SwingConstants.RIGHT), c);
+		c.gridwidth = 3;
+		c.gridx = 1;
+		c.anchor = GridBagConstraints.LINE_START;
+		dialog.add(location, c);
+		c.gridwidth = 1;
+		c.gridx = 0;
+		c.gridy = 11;
+		c.anchor = GridBagConstraints.LINE_END;
+		dialog.add(new JLabel("Free", SwingConstants.RIGHT), c);
+		c.gridx = 1;
+		c.anchor = GridBagConstraints.LINE_START;
+		dialog.add(isAvailable, c);
+		c.gridx = 0;
+		c.gridy = 12;
+		c.gridwidth = 4;
+		c.anchor = GridBagConstraints.CENTER;
+		dialog.add(new JLabel("Description", SwingConstants.CENTER), c);
+		c.gridy = 13;
+		dialog.add(description, c);
+		c.gridy = 14;
+		dialog.add(new JLabel("Attendees", SwingConstants.CENTER), c);
+		c.gridy = 15;
+		c.gridwidth = 1;
+		c.anchor = GridBagConstraints.LINE_END;
+		dialog.add(new JLabel("Email", SwingConstants.RIGHT), c);
+		c.gridx = 1;
+		c.gridwidth = 2;
+		c.anchor = GridBagConstraints.LINE_START;
+		dialog.add(attendee, c);
+		c.gridwidth = 1;
+		c.gridx = 3;
+		dialog.add(addAttendee, c);
+		c.gridx = 0;
+		c.gridy = 16;
+		c.gridwidth = 2;
+		c.anchor = GridBagConstraints.CENTER;
+		c.insets = new Insets(1,10,20,10);
+		dialog.add(create, c);
+		c.gridx = 2;
+		dialog.add(cancel, c);
+
+		dialog.pack();
+		dialog.setVisible(true);
 	}
 
 	private void loadFile() {
